b934e5033b5b1e628f2345e5683e23817cdc8d4a
latishasumardy <sumardy5031@mydusd.org>
2018-01-16 09:59:11 -0800

    Almost done

--- app/src/main/java/examples/pltw/org/collegeapp/ApplicantData.java
+++ app/src/main/java/examples/pltw/org/collegeapp/ApplicantData.java
@@ -5,4 +5,13 @@ package examples.pltw.org.collegeapp;
  */
 
 public class ApplicantData {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
